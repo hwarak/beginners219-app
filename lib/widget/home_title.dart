91063cf9252e291e8f9ad97181b36cf38b53cbd7
hwarak <hwarakhada@gmail.com>
2022-10-17 01:53:27 +0900

    decoration home title

--- lib/widget/home_title.dart
+++ lib/widget/home_title.dart
@@ -6,11 +6,25 @@ class HomeTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: TextStyle(
-        fontSize: 18.0,
-        fontWeight: FontWeight.w700,
+    return Container(
+      height: 30.0,
+      child: Stack(
+        children: [
+          Positioned(
+            top: 17.0,
+            child: Image.asset(
+              "assets/images/home_title_underline.jpg",
+              width: title == "Main Article" ? 143.0 : 200.0,
+            ),
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
       ),
     );
   }
