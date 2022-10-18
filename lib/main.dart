03762448c67a560771cf750bc6775fdf8e2163a2
hwarak <hwarakhada@gmail.com>
2022-10-19 00:51:42 +0900

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'layout/default_layout.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
   await dotenv.load(fileName: 'assets/config/.env');
+
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(MaterialApp(
     home: DefaultLayout(),
     debugShowCheckedModeBanner: false,
   ));
+
+  FlutterNativeSplash.remove();
 }
